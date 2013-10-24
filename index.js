5085b8368e4c230c489c0d3fbee5ce4b4dde9bab
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2013-10-24 09:18:56 -0400

    measuring jshint coverage

--- index.js
+++ index.js
@@ -18,18 +18,41 @@ function getAllSettings() {
   return allSettings;
 }
 
-var allSettings = getAllSettings();
-check.verifyObject(allSettings, 'could not get all jshint settings');
-
 // returns percent 0 - no valid settings, 100 - all settings specified
 function settingsPercentage(projectJshintSettings) {
   check.verifyObject(projectJshintSettings, 'expected jshint object');
 
-  return 50;
+  //console.log('looking at jshint settings\n' +
+  //  JSON.stringify(projectJshintSettings, null, 2));
+
+  var allSettings = getAllSettings();
+  check.verifyObject(allSettings, 'could not get all jshint settings');
+  var totalSettings = Object.keys(allSettings).length;
+  check.verifyPositiveNumber(totalSettings, 'epected all settings to have properties');
+
+  Object.keys(projectJshintSettings).forEach(function (key) {
+    if (typeof allSettings[key] === 'undefined') {
+      console.error('unknown setting', key, projectJshintSettings[key]);
+    }
+  });
+
+  var specifiedSettings = Object.keys(projectJshintSettings).length;
+
+  return (specifiedSettings / totalSettings * 100).toPrecision(2);
 }
 
 var jshintFilename = './.jshintrc';
 if (!fs.existsSync(jshintFilename)) {
   check.verifyUnemptyString(jshintFilename, 'expected jshint filename');
   throw new Error('Cannot find jshint settings file ' + jshintFilename);
+}
+var settings = JSON.parse(fs.readFileSync(jshintFilename, 'utf-8'));
+check.verifyObject(settings, 'expected jshint settings object');
+
+var quality = settingsPercentage(settings);
+console.assert(quality >= 0 && quality <= 100, 'invalid quality value ' + quality);
+console.log('jshint', jshintFilename, 'covers', quality + '% of all possible settings');
+if (quality < 75) {
+  console.log('see all possible settings at\n' +
+    'https://raw.github.com/jshint/jshint/master/examples/.jshintrc');
 }
