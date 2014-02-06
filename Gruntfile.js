3b0f04d54371469d8dbce1ad2b91b82580ebaa93
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2014-02-06 15:02:54 -0500

    added grunt and jshint, fixes #6

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = function(grunt) {
+  grunt.initConfig({
+    jshint: {
+      all: [
+        '*.js'
+      ],
+      options: {
+        jshintrc: '.jshintrc',
+        reporter: require('jshint-stylish')
+      },
+    },
+
+    'nice-package': {
+      all: {
+        options: {
+        }
+      }
+    }
+  });
+
+  var plugins = require('matchdep').filterDev('grunt-*');
+  plugins.forEach(grunt.loadNpmTasks);
+  grunt.registerTask('default', ['jshint']);
+};
